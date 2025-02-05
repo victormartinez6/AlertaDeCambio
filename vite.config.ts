2edd7286f62609ad0d5274250adce59a305fc948
victormartinez6 <victormartinez6@gmail.com>
2025-02-05 16:45:23 -0500

    Simplifica configurações do Vite e Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    port: 8080,
-    host: 'localhost',
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false
-  },
-  base: '/'
+  }
 })
