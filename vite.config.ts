cab53d1a1e2f22baaa9eb22ee635042f943366ec
victormartinez6 <victormartinez6@gmail.com>
2025-02-05 16:41:57 -0500

    Ajusta configurações para SPA na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -13,5 +14,12 @@ export default defineConfig({
     port: 8080,
     host: 'localhost',
     open: true
-  }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  base: '/'
 })
